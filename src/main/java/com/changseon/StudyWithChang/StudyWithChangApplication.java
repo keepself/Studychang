17de366c7f39a5fa8121be7f734568ecbe11ff54
이창선 <changman@MacBook-Pro-som-tillhor-ichangseon.local>
2024-08-27 17:13:16 +0900

    second message

---
+++ src/main/java/com/changseon/StudyWithChang/StudyWithChangApplication.java
@@ -0,0 +1,13 @@
+package com.changseon.StudyWithChang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyWithChangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyWithChangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/changseon/StudyWithChang/StudyWithChangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changseon.StudyWithChang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyWithChangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
